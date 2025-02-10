e4a7872327cee19ec07525a510522dddaa8ab093
Daniil <photo.raz@yandex.ru>
2025-02-10 11:30:07 +0300

    Для создания таблиц базы данных

---
+++ src/DB/create_db.sql
@@ -0,0 +1,18 @@
+CREATE TABLE IF NOT EXISTS virtual_machines (
+    id SERIAL PRIMARY KEY,
+    vm_id UUID NOT NULL UNIQUE,
+    ram INTEGER NOT NULL CHECK (ram > 0),
+    cpu INTEGER NOT NULL CHECK (cpu > 0)
+);
+
+CREATE TABLE IF NOT EXISTS disks (
+    id SERIAL PRIMARY KEY,
+    disk_id UUID NOT NULL UNIQUE,
+    vm_id UUID REFERENCES virtual_machines(vm_id) ON DELETE CASCADE,
+    size INTEGER NOT NULL CHECK(size > 0)
+);
+
+CREATE TABLE IF NOT EXISTS vm_sessions (
+    id serial PRIMARY KEY,
+    vm_id UUID REFERENCES virtual_machines(vm_id) ON DELETE CASCADE
+);
