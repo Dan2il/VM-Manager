d369f04a93dcf23f65c8998ae5529cea5ae7bbb7
Daniil <photo.raz@yandex.ru>
2025-02-10 11:31:23 +0300

    Файл для настроек сервера

---
+++ src/server/setting.py
@@ -0,0 +1,12 @@
+HOST = "0.0.0.0"
+PORT = 8888
+DB_CONFIG = {
+    "user": "admin",
+    "password": "admin",
+    "database": "vm_manager",
+    "host": "localhost",
+    "port": 5432,
+}
+
+
+MESSAGE_LIMIT = 1024
