30b6a3c9cce460eb72a7f10a9dda4cd3632eb741
unknown <photo.raz@yandex.ru>
2025-02-10 19:07:44 +0300

    изменен тип данных и добавлены магические методы

--- src/server/virtual_machine.py
+++ src/server/virtual_machine.py
@@ -1,11 +1,18 @@
 from pydantic import BaseModel, Field
-
+from uuid import UUID
 
 class VirtualMachine(BaseModel):
     """Класс, представляющий собой виртуальную машину"""
     
-    vm_id: int
+    vm_id: UUID
     ram: int = Field(gt=0)
     cpu: int = Field(gt=0)
-    disks: dict[int, int]
+    disks: dict[UUID, int]
 
+
+    def __repr__(self):
+        return f"{self.vm_id}:{self.ram}:{self.cpu}:{self.disks}"
+
+    def __str__(self):
+        return (f"VM:\nid: {self.vm_id},\nram: {self.ram},\n"
+                f"cpu: {self.cpu},\ndisks: {self.disks}\n")
